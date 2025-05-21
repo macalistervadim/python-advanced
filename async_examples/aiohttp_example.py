49ecdf7b773c903dfc99295abf917df22c8e87f8
macalistervadim <macalistervadim@yandex.ru>
2025-05-21 17:27:38 +0600

    add multiproc_examples

--- async_examples/aiohttp_example.py
+++ async_examples/aiohttp_example.py
@@ -6,17 +6,18 @@ from async_timed import async_timed
 
 @async_timed()
 async def fetch_status(session: ClientSession, url: str) -> int:
-    ten_millis = aiohttp.ClientTimeout(total=.01)
+    ten_millis = aiohttp.ClientTimeout(total=10)
     async with session.get(url, ssl=False, timeout=ten_millis) as response:
         return response.status
     
     
 @async_timed()
 async def main():
-    session_timeout = aiohttp.ClientTimeout(total=1, connect=.1)
+    session_timeout = aiohttp.ClientTimeout(total=10, connect=.1)
     async with aiohttp.ClientSession(timeout=session_timeout) as session:
-        url = "https://www.google.com"
-        status = await fetch_status(session, url)
-        print(f"Status code for {url}: {status}")
+        urls = ["https://example.com" for _ in range(1000)]
+        requests = [fetch_status(session, url) for url in urls]
+        status_code = await asyncio.gather(*requests)
+        print(status_code)
         
 asyncio.run(main())
